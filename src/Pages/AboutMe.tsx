1b000f82057e57ea4e0388d9c2cd3d24886b9648
Anchamdu <aignacio.chamorro.duque@gmail.com>
2023-07-24 11:22:26 +0200

    .

--- src/Pages/AboutMe.tsx
+++ src/Pages/AboutMe.tsx
@@ -16,7 +16,7 @@ const AboutMe = () => {
         <h1 className="text-5xl font-bold text-gray-900 mb-20">About Me</h1>
 
         <p className="text-xl text-gray-700 mb-8">Raised in Extremadura, Spain, I'm passionate assionate about technology, music, and video games. Trained in <span className="font-bold italic text-red-400">English</span> and  <span className="font-bold italic text-red-400">Law</span>, with experience as an <span className="font-bold italic text-red-400">Educator</span>.</p>
-        <p className="text-xl text-gray-700 mb-8">Embarking on a journey into the world of <span className="font-bold italic text-red-400">programming. </span> Learning SQL, Java, Javascript, React, HTML, and CSS, I aspire to become a <span className="font-bold italic text-red-400">Full Stack developer. </span></p>
+        <p className="text-xl text-gray-700 mb-8">Embarking on a journey into the world of <span className="font-bold italic text-red-400">programming. </span> Learning SQL, Java, Javascript, React, HTML, CSS... I aspire to become a <span className="font-bold italic text-red-400">Full Stack developer. </span></p>
         <p className="text-xl text-gray-700 mb-8">Creative, persevering, and focused on tackling <span className="font-bold italic text-red-400">challenges.</span></p>
         <p className="text-xl text-gray-700 mb-8">Sharing my progress and projects on this landing page.</p>
         <p className="text-xl text-gray-700 mb-8"><span className="font-bold italic text-red-400">Join me</span> on this exciting adventure into technological development.</p>
